de506a6a8cd2c940d289e2762f0ac972f1e9840f
francisyzy <francisyzy@me.com>
2022-09-25 15:38:12 +0800

    Add notify fetchur command

--- src/commands/helper.ts
+++ src/commands/helper.ts
@@ -1,16 +1,25 @@
 import bot from "../lib/bot";
 import { getBotCommands } from "../utils/botCommands";
+import { notifyFetchur } from "../utils/checkFetchur";
 
 //General helper commands
 const helper = () => {
-
   //All bots start with /start
   bot.start((ctx) => {
     ctx.setMyCommands(getBotCommands());
-    return ctx.reply("Welcome to jacob event bot.")
+    return ctx.reply("Welcome to jacob event bot.");
   });
 
   bot.help((ctx) => ctx.reply("Help message"));
+
+  bot.command("updateFetchur", (ctx) => {
+    try {
+      notifyFetchur();
+      return ctx.reply("Updated");
+    } catch (error) {
+      return ctx.reply("Failed");
+    }
+  });
 };
 
 export default helper;
