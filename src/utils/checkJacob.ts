19e3687cadc6e0d3f2d19861c271241b00d318fb
francisyzy <francisyzy@me.com>
2022-11-03 15:54:53 +0800

    Fix 404 err thingy

--- src/utils/checkJacob.ts
+++ src/utils/checkJacob.ts
@@ -11,36 +11,42 @@ import { events } from "../types";
 
 export async function notifyJacob(): Promise<void> {
   console.log("Notifying jacob");
-  const events = (await got(
-    "https://raw.githubusercontent.com/francisyzy/data-skyblock/gh-pages/jacob-events.min.json",
-  ).json()) as events;
   let notifier: {
     cropMessage: string;
     startingIn: string;
     mcDate: string;
   }[] = [];
-  events.forEach((event) => {
-    if (
-      isWithinInterval(new Date(event.rlDate), {
-        start: subMinutes(new Date(), config.eventBuffer),
-        end: addMinutes(new Date(), config.eventBuffer),
-      })
-    ) {
-      const cropMessage = event.crops.join(", ");
-      const startingIn = formatDistanceToNow(new Date(event.rlDate));
-      notifier.push({
-        cropMessage: cropMessage,
-        startingIn: startingIn,
-        mcDate: event.mcDate,
-      });
-    }
-  });
-  //Removes duplicate events
-  //https://stackoverflow.com/a/36744732
-  notifier = notifier.filter(
-    (value, index, self) =>
-      index === self.findIndex((t) => t.mcDate === value.mcDate),
-  );
+  try {
+    const events = (await got(
+      "https://raw.githubusercontent.com/francisyzy/data-skyblock/gh-pages/jacob-events.min.json",
+    ).json()) as events;
+    events.forEach((event) => {
+      if (
+        isWithinInterval(new Date(event.rlDate), {
+          start: subMinutes(new Date(), config.eventBuffer),
+          end: addMinutes(new Date(), config.eventBuffer),
+        })
+      ) {
+        const cropMessage = event.crops.join(", ");
+        const startingIn = formatDistanceToNow(
+          new Date(event.rlDate),
+        );
+        notifier.push({
+          cropMessage: cropMessage,
+          startingIn: startingIn,
+          mcDate: event.mcDate,
+        });
+      }
+    });
+    //Removes duplicate events
+    //https://stackoverflow.com/a/36744732
+    notifier = notifier.filter(
+      (value, index, self) =>
+        index === self.findIndex((t) => t.mcDate === value.mcDate),
+    );
+  } catch (error) {
+    console.log(error);
+  }
   //Sends notification out
   notifier.forEach((notify) => {
     bot.telegram.sendMessage(
@@ -53,7 +59,11 @@ export async function notifyJacob(): Promise<void> {
     bot.telegram.sendMessage(
       config.JACOB_TG_CHANNEL_ID,
       `Jacob's Farming Contest is starting soon. <i>Unable to get crops as the <a href="https://hypixel-skyblock.fandom.com/wiki/Jacob%27s_Farming_Contest/Events">Wiki</a> is yet to be updated</i>`,
-      { parse_mode: "HTML", disable_web_page_preview: true, disable_notification: true },
+      {
+        parse_mode: "HTML",
+        disable_web_page_preview: true,
+        disable_notification: true,
+      },
     );
   }
 }
